f5ef7884650c8f808ba8022c744916149f9004b9
Kirito1610 <vishalkundu1610@gmail.com>
2025-04-25 23:25:23 +0530

    herosection modifictaion

--- src/Component/HomePage/Herosection.js
+++ src/Component/HomePage/Herosection.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import HeroRightBox from './HeroRight';
+import HerSectionSlider from './HerSectionSlider';
 const Herosection = () => {
   return (
-    <div className="w-full h-[50vh] md:h-[70vh]">
-      <div
+    <div className="w-full bg-[#FBE4D6]">
+      {/* <div
         className="w-full h-full flex items-center justify-center md:justify-start"
         style={{
           backgroundImage: "url('https://watermark.lovepik.com/photo/20211118/large/lovepik-e-commerce-merchandise-womens-bag-shooting-picture_500124773.jpg')",
@@ -13,7 +14,8 @@ const Herosection = () => {
         }}
       >
         <HeroRightBox />
-      </div>
+      </div> */}
+      <HerSectionSlider />
     </div>
   );
 };
